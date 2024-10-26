52ab308e73e587d375987dbd5d29e3486706f3ac
le-huy-jh <74605855+yuhlee610@users.noreply.github.com>
2024-10-26 15:23:54 +0700

    feat: Add manga type

--- src/app/models/mangadex.ts
+++ src/app/models/mangadex.ts
@@ -1,5 +1,237 @@
-import { Links } from './static';
+import { Includes, Links, MangaContentRating, MangaPublicationDemographic, MangaPublicationStatus, MangaReadingStatus, Order } from './static';
 import { AuthenticationToken } from './auth';
+
+/*******************
+ * TYPE DEFINITIONS
+ *******************/
+
+/** Aggregate chapter type used in AggregateVolume for GetMangaIdAggregateResponse */
+export type AggregateChapter = {
+  chapter: string;
+  /** UUID formatted string */
+  id: string;
+  /** UUID formatted strings */
+  others: string[];
+  /** Total number of chapters across filtered languages */
+  count: number;
+};
+
+/** Aggregate volume type for GetMangaIdAggregateResponse */
+export type AggregateVolume = {
+  volume: string;
+  /** Total number of chapters in volume across filtered languages */
+  count: number;
+  chapters: Record<string, AggregateChapter>;
+};
+
+/** Order object for GetSearchMangaRequestOptions */
+export type GetSearchMangaOrder = {
+  title?: Order;
+  year?: Order;
+  createdAt?: Order;
+  updatedAt?: Order;
+  latestUploadedChapter?: Order;
+  followedCount?: Order;
+  relevance?: Order;
+  rating?: Order;
+};
+
+/** Order object for GetMangaIdFeedRequestOptions */
+export type GetMangaIdFeedOrder = {
+  createdAt?: Order;
+  updatedAt?: Order;
+  publishAt?: Order;
+  readableAt?: Order;
+  volume?: Order;
+  chapter?: Order;
+};
+
+/***********************
+ * API REQUEST/RESPONSE
+ ***********************/
+
+/** Request parameters for `GET /manga` */
+export type GetSearchMangaRequestOptions = {
+  /** Default: 10 */
+  limit?: number;
+  offset?: number;
+  title?: string;
+  authors?: string[];
+  artists?: string[];
+  /** Year of release */
+  year?: number;
+  includedTags?: string[];
+  /** Default: AND */
+  includedTagsMode?: "AND" | "OR";
+  excludedTags?: string[];
+  /** Default: OR */
+  excludedTagsMode?: "AND" | "OR";
+  status?: MangaPublicationStatus[];
+  /** ISO 639-1 standard two or five letter language code */
+  originalLanguage?: string[];
+  /** ISO 639-1 standard two or five letter language code */
+  excludedOriginalLanguage?: string[];
+  /** ISO 639-1 standard two or five letter language code */
+  availableTranslatedLanguage?: string[];
+  publicationDemographic?: MangaPublicationDemographic[];
+  /** Manga IDs, limited to 100 per request */
+  ids?: string[];
+  /** Default: ['safe', 'suggestive', 'erotica'] */
+  contentRating?: MangaContentRating[];
+  /** DateTime string with following format: YYYY-MM-DDTHH:mm:SS */
+  createdAtSince?: string;
+  /** DateTime string with following format: YYYY-MM-DDTHH:mm:SS */
+  updatedAtSince?: string;
+  /** Default:
+   * ```
+   * { latestUploadedChapter: 'desc' }
+   * ``` */
+  order?: GetSearchMangaOrder;
+  includes?: Includes[];
+  hasAvailableChapters?: "0" | "1" | "true" | "false";
+  /** UUID formatted string */
+  group?: string;
+};
+
+/** Response from `GET /manga` */
+export type GetSearchMangaResponse = MangaList;
+
+/** Response from `GET /manga/status` */
+export type GetMangaStatusResponse = {
+  /** Default: "ok" */
+  result: string;
+  /**
+   * Property names in `statuses` are UUID formatted strings
+   *
+   * Example:
+   * ```
+   * {
+   *     "result": "ok",
+   *     "statuses": {
+   *         "b019ea5d-5fe6-44d4-abbc-f546f210884d": "reading"
+   *         "2394a5c7-1d2e-461f-acde-18726b9e37d6": "dropped"
+   *     }
+   * }
+   * ```
+   */
+  statuses: Record<string, MangaReadingStatus>;
+};
+
+/** Request parameters for `GET /manga/{id}/feed` */
+export type GetMangaIdFeedRequestOptions = {
+  /**
+   * ```console
+   * Default: 100
+   * Minimum: 1
+   * Maximum: 500
+   * ```
+   */
+  limit?: number;
+  /** Minimum: 0 */
+  offset?: number;
+  /** ISO 639-1 standard two or five letter language code */
+  translatedLanguage?: string[];
+  /** ISO 639-1 standard two or five letter language code */
+  originalLanguage?: string[];
+  /** ISO 639-1 standard two or five letter language code */
+  excludedOriginalLanguage?: string[];
+  contentRating?: MangaContentRating[];
+  /** UUID formatted strings */
+  excludedGroups?: string[];
+  /** UUID formatted strings */
+  excludedUploaders?: string[];
+  /** Default: 1 */
+  includeFutureUpdates?: "0" | "1";
+  /** DateTime string with format YYYY-MM-DDTHH:mm:SS */
+  createdAtSince?: string;
+  /** DateTime string with format YYYY-MM-DDTHH:mm:SS */
+  updatedAtSince?: string;
+  /** DateTime string with format YYYY-MM-DDTHH:mm:SS */
+  publishAtSince?: string;
+  order?: GetMangaIdFeedOrder;
+  includes?: Includes[];
+};
+
+/** Response from `GET /manga/{id}/feed` */
+export type GetMangaIdFeedResponse = ChapterList;
+
+/** Request parameters for `GET /manga/{id}/aggregate` */
+export type GetMangaIdAggregateRequestOptions = {
+  /** ISO 639-1 standard two or five letter language code */
+  translatedLanguage?: string[];
+  /** UUID formatted strings */
+  groups?: string[];
+};
+
+/** Response from `GET /manga/{id}/aggregate` */
+export type GetMangaIdAggregateResponse = {
+  /** Default: "ok" */
+  result: string;
+  /** Object containing volumes and their respective chapters */
+  volumes: Record<string, AggregateVolume>;
+};
+
+/** Request parameters for `GET /manga/{id}` */
+export type GetMangaIdRequestOptions = {
+  includes?: Includes[];
+};
+
+/** Response from `GET /manga/{id}` */
+export type GetMangaIdResponse = MangaResponse;
+
+/** Response from `GET /manga/{id}/read` */
+export type GetMangaIdReadMarkersResponse = {
+  result: "ok";
+  /** UUID formatted strings */
+  data: string[];
+};
+
+/** Request parameters for `GET /manga/read` */
+export type GetMangaReadMarkersRequestOptions = {
+  /** UUID formatted strings */
+  ids: string[];
+  /** Group results by manga ID */
+  grouped?: boolean;
+};
+
+/** Response from `GET /manga/read` */
+export type GetMangaReadMarkersResponse = {
+  result: "ok";
+  /**
+   * Type is dependent on value passed into request options `grouped`
+   *
+   * For example:
+   *
+   * ```console
+   * grouped: true
+   * data: {
+   *     1234abcd-0000-0000-0000-1234abcd5678: [
+   *         'abcd1234-1111-1111-1111-abcd1234efab',
+   *         'abcd1234-2222-2222-2222-2468acea0246'
+   *     ]
+   * }
+   *
+   * grouped: false
+   * data: [
+   *     'abcd1234-1111-1111-1111-abcd1234efab',
+   *     'abcd1234-2222-2222-2222-2468acea0246'
+   * ]
+   * ```
+   */
+  data: string[] | Record<string, string>[];
+};
+
+/** Request parameters for `GET /manga/random` */
+export type GetMangaRandomRequestOptions = {
+  includes?: Includes[];
+  contentRating?: MangaContentRating[];
+};
+
+/** Response from `GET /manga/random` */
+export type GetMangaRandomResponse = MangaResponse;
+
+/** Response from `GET /manga/tag` */
+export type GetMangaTagResponse = TagResponse;
 
 /**
  * Makes named properties required in pre-existing type. All other properties are made optional.
