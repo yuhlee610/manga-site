30a85ecaf47f9c174d1918b09e3bf83a19b61a8d
huy.le <yuhlee610@gmail.com>
2024-11-11 21:03:53 +0700

    feat: Add link for tag

--- src/app/shared/components/tag/tag.component.ts
+++ src/app/shared/components/tag/tag.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NzTagModule } from 'ng-zorro-antd/tag';
 
 @Component({
   selector: 'app-tag',
   standalone: true,
-  imports: [NzTagModule],
+  imports: [NzTagModule, RouterLink],
   templateUrl: './tag.component.html',
   styleUrl: './tag.component.scss',
 })
-export class TagComponent {}
+export class TagComponent {
+  tagId = input();
+}
